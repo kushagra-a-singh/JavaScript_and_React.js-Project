97cde4fdc345e1708c77718ba58c96cd8625093e
Kushagra Anit Singh <105034224+kushagra-a-singh@users.noreply.github.com>
2023-01-17 23:03:25 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const currentyear = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p> Copyright @ {currentyear} </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> About Me--> </h1>
+      <p>
+        Currently, pursuing BTech in Computer Science Engineering from MIT WPU,
+        Pune. Check-out project source codes here---
+        https://github.com/kushagra-a-singh | My Social Handles---
+        https://linktr.ee/kushagra_singh
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
